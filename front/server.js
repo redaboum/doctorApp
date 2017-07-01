24d06441107c004b3462d5fd8b9bf690521c7a67
redaboum <redab@sicara.com>
2017-07-01 22:07:12 +0200

    b

--- front/server.js
+++ front/server.js
@@ -6,9 +6,9 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(5000, 'localhost', (err) => {
+}).listen(3000, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
-  console.log('Listening at localhost:5000');
+  console.log('Listening at localhost:3000');
 });
