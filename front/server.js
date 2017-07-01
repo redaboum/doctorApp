6d166fbc1a63d966f462e71658131329eb2f96cb
redaboum <redab@sicara.com>
2017-07-01 22:11:36 +0200

    t

--- front/server.js
+++ front/server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', (err) => {
+}).listen(80, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
